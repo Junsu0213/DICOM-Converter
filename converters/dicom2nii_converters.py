dfac9414a75be60b9381976caea6825e137e7441
Junsu Park <128777619+Junsu0213@users.noreply.github.com>
2024-08-28 17:30:12 +0900

    Create dicom2nii_coverters.py

---
+++ converters/dicom2nii_converters.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+"""
+Created on Wed. Aug. 28 17:24:07 2024
+@author: JUN-SU Park
+"""
+import SimpleITK as sitk
+import os
+
+
+def convert_dicom_to_nii(input_dir, output_dir=None):
+    if output_dir is None:
+        parent_dir = os.path.dirname(input_dir)
+        output_dir = os.path.join(parent_dir, 'nii')
+    os.makedirs(output_dir, exist_ok=True)
+    
+    file_name = os.path.basename(input_dir)
+
+    reader = sitk.ImageSeriesReader()
+    dicom_names = reader.GetGDCMSeriesFileNames(input_dir)
+    reader.SetFileNames(dicom_names)
+    image = reader.Execute()
+    sitk.WriteImage(image, f'{output_dir}/{file_name}.nii.gz')
