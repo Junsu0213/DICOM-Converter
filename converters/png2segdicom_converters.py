1c342b24248abaeabde9297dfb7027e6b369c4b8
Junsu Park <128777619+Junsu0213@users.noreply.github.com>
2024-08-30 16:20:13 +0900

    edit_png2segdicom_converters

--- converters/png2segdicom_converters.py
+++ converters/png2segdicom_converters.py
@@ -31,6 +31,7 @@ def convert_png_to_segdicom(png_name, labels, input_dir, output_dir=None):
     initial_series_number = None
 
     ref_dcm_files = sorted(os.listdir(dcm_dir))
+    print(ref_dcm_files)
 
     for dcm_file in ref_dcm_files:
         if dcm_file.endswith('.dcm'):
@@ -41,15 +42,25 @@ def convert_png_to_segdicom(png_name, labels, input_dir, output_dir=None):
                 # Read reference DICOM to check series number
                 dcm_ref = pydicom.dcmread(dcm_ref_file)
                 current_series_number = dcm_ref.SeriesNumber
+                print(initial_series_number, current_series_number)
 
                 # If this is the first file, set the initial series number
                 if initial_series_number is None:
                     initial_series_number = current_series_number
 
                 # Check if series number has changed
-                if current_series_number != initial_series_number:
+                if current_series_number != initial_series_number and series_num > 5:
+                    print(series_num)
                     print(f"Series number changed. Stopping processing")
                     break
+                elif current_series_number != initial_series_number and series_num < 5:
+                    for file in temp_dcm_files:
+                        os.remove(file)
+                        temp_dcm_files.pop()
+                    temp_dcm_files = []
+
+                if initial_series_number != current_series_number:
+                    initial_series_number = current_series_number
 
                 series_num += 1
                 convert_png_to_dicom(png_file, dcm_ref_file, temp_dcm_file)
--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # -*- coding:utf-8 -*-
+import pydicom
 
 if __name__ == '__main__':
-    print('test')
+    dcm_path = r'D:\DATASET\COVID_CT_Dataset\COV-CCO\COV-CCO-001\20210112\dcm\00000284.dcm'
+    dcm = pydicom.dcmread(dcm_path)
+    print(dcm)
